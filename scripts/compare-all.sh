8bb6ad92438566d93145b2b14c680692a1a46875
Youssef-Bahy-Youssef <youssefbahy2022@gmail.com>
2025-04-19 18:00:37 +0200

    solve R8 error - needed test

--- scripts/compare-all.sh
+++ scripts/compare-all.sh
@@ -4,3 +4,5 @@
 ./compare/pipeline-test.sh
 ./compare/shader-test.sh
 ./compare/transform-test.sh
+./compare/texture-test.sh
+./compare/sampler-test.sh
---
+++ scripts/compare/entity-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./test-images.sh \
+  "entity-test" \
+  0.01 \
+  0 \
+  "test-0.png" "test-1.png"
